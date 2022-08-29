dd55d19187b0a513d9a2ecec8fea479604993355
alvaroormeno <alvaro@ormeno.org>
2022-08-28 23:37:16 -0400

    installed date-fns, added date to posts from initial state and prepare in reducer

--- src/features/posts/postsSlice.js
+++ src/features/posts/postsSlice.js
@@ -1,16 +1,19 @@
 import { createSlice, nanoid } from "@reduxjs/toolkit";
+import { sub } from "date-fns";
 
 
 const initialState = [
   {
     id: '1',
     title: 'Learning Redux Toolkit',
-    content: ' Ive heard things about redux'
+    content: ' Ive heard things about redux',
+    date: sub(new Date(), {minutes: 10}).toISOString()
   },
   {
     id: '2',
     title: 'Slices...',
-    content: ' Ive heard things about slices!'
+    content: ' Ive heard things about slices!',
+    date: sub(new Date(), {minutes: 5}).toISOString()
   }
 ]
 
@@ -39,6 +42,7 @@ const postsSlice = createSlice({
             id: nanoid(),
             title,
             content,
+            date: new Date().toISOString(),
             userId
           }
         }
