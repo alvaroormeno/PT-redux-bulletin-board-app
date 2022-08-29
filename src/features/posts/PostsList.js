b49a6bebbe8c20d399f9fa0a5738853cd8287dde
alvaroormeno <alvaro@ormeno.org>
2022-08-28 23:51:01 -0400

    postsLists.js - sorted posts based on date and saved them into orderedPosts variable and now mapping this new array of ordered posts

--- src/features/posts/PostsList.js
+++ src/features/posts/PostsList.js
@@ -12,8 +12,12 @@ const PostsList = () => {
   // Instead of using this: const posts = useSelector(state => state.posts), we created a selector inside postsSlice which we imported for best practice if we ever change the state inside postsSlice.js
   const posts = useSelector(selectAllPosts)
 
+  // Here with slice we are creating a copy of the posts array and saving it to orderedposts
+  // With sort, we are doing an a b comparing based on the date, locale compare will return a value of -1, 1 or 0 to so they can be sorted.
+  const orderedPosts = posts.slice().sort((a, b) => b.date.localeCompare(a.date))
+
   //Map posts variable whis has the posts data from initial state
-  const renderedPosts = posts.map(post => (
+  const renderedPosts = orderedPosts.map(post => (
     <article key={post.id}>
       <h3>{post.title}</h3>
       {/* With substring we are getting the first 100 characters of the post, in other words a preview of the post content. */}
