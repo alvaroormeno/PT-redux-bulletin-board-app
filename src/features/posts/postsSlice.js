7d75f89b7b901df2bf5b4a6032ac8066b865dc63
alvaroormeno <alvaro@ormeno.org>
2022-08-28 22:01:42 -0400

    created new reducer function postAdded to receive posts from form component and push it to our state

--- src/features/posts/postsSlice.js
+++ src/features/posts/postsSlice.js
@@ -17,11 +17,16 @@ const postsSlice = createSlice({
   name: 'posts',
   initialState,
   reducers: {
-
+    postAdded(state, action) {
+      state.push(action.payload)
+    }
   }
 })
 
 // Create selector to export and be used in components, this way if the state ever changed we only need to change this selector and not the selector inside every component.
 export const selectAllPosts = (state) => state.posts;
+
+// Export actons
+export const { postAdded } = postsSlice.actions
 
 export default postsSlice.reducer
