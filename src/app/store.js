4aa1e0e7049c6acc8017c8cbda1ea037fd90d00c
alvaroormeno <alvaro@ormeno.org>
2022-08-28 22:52:53 -0400

    imported usersReducer to store

--- src/app/store.js
+++ src/app/store.js
@@ -2,10 +2,12 @@ import { configureStore } from "@reduxjs/toolkit";
 
 // Import Reducer
 import postsReducer from '../features/posts/postsSlice.js'
+import usersReducer from '../features/users/usersSlice.js'
 
 
 export const store = configureStore({
     reducer: {
         posts: postsReducer,
+        users: usersReducer
     }
 })
