35c29fb6ea7f9fa66f7b977ee62ab1b0fdef9fbb
alvaroormeno <alvaro@ormeno.org>
2022-08-28 21:38:35 -0400

    store.js, imported posts reducer

--- src/app/store.js
+++ src/app/store.js
@@ -1,8 +1,11 @@
 import { configureStore } from "@reduxjs/toolkit";
 
+// Import Reducer
+import postsReducer from '../features/posts/postsSlice.js'
+
 
 export const store = configureStore({
     reducer: {
-
+        posts: postsReducer,
     }
 })
